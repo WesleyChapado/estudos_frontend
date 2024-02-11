ab79ea1e7cc4ea553c912d154e7fd9b91f5c9ffa
Wesley Chapado <wesley-chapado@hotmail.com>
2024-02-11 02:11:40 -0300

    Adicionando aulas.

---
+++ javascript/tipos_de_saida/tipos_de_saida.js
@@ -0,0 +1,9 @@
+// getElementById = obtém um elemento da página pelo id.
+// innerHTML = envia informação em HTML para o elemento selecionado.
+document.getElementById("texto_innerhtml").innerHTML = "Exemplo de texto usando innerHTML";
+
+// faz aparecer um popup com um alerta
+// alert("Exemplo de alerta");
+
+// envia uma mensagem para o console(inspecionar)
+console.log("exemplo de console log");
